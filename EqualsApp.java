65dda8e307c6d592b028965c13eb66237a9671af
EverythingForJ <67085700+EverythingForJ@users.noreply.github.com>
2020-07-01 12:32:04 +0900

    JavaFlowControl
    FlowControl

---
+++ EqualsApp.java
@@ -0,0 +1,12 @@
+
+public class EqualsApp {
+
+	public static void main(String[] args) {
+		// ==는 위치가 같은지 확인. equals는 계산하여 동일하게 확인
+
+		String o1 = "java";
+		String o2 = new String("java");
+		
+	}
+
+}
---
+++ LoopArray.java
@@ -0,0 +1,22 @@
+
+public class LoopArray {
+
+	public static void main(String[] args) {
+		
+		String[] users = new String[3];
+		
+		users[0] = "Junhwan";
+		users[1] = "Egoing";
+		users[2] = "Java";
+		
+		for(int i=0; i<users.length; i++) {
+			System.out.println("<li>"+users[i]+"</li>");
+			
+		}
+		
+	
+	
+	
+	}
+
+}
